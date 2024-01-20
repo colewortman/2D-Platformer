8e59ddc6d4efacce75b74854bf1884ab315524a3
colewortman <wortcole12@gmail.com>
2024-01-19 18:43:50 -0600

    Updated First Boss
    Made a lot of progress in debugging.

--- scripts/colossus_knight.gd
+++ scripts/colossus_knight.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
-@onready var player = $/root/World/Player
+@onready var player = $/root/Death_void/Player
 @onready var anim = $AnimatedSprite2D
-@onready var loot_base = $/root/World/loot
+#@onready var loot_base = $/root/World/loot
 
 @export var attack_damage: float
 @export var knockback_force: float
 @export var stun_damage: float
 
-var speed = 10.0
+var speed = 0.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var soul = preload("res://scenes/souls.tscn")
 @export var experience := 2
@@ -25,22 +25,29 @@ enum states{
 	
 	IDLE,
 	CHARGE,
+	BASH,
 	SLAM,
 	DEATH
 	
 }
 
 func _process(delta):
-	
 	if !state == states.DEATH and $Death_timer.is_stopped() == true:
 		handle_physics(delta)
 		handle_flipping()
 		if player_far and attack_ip == false:
+			attack_ip = true
 			state = states.CHARGE
+			$Shield/shield_hitbox.disabled = false
+			$charge_timer.start()
+		elif player_far == false and attack_ip == false:
 			attack_ip = true
-		elif attack_ip == false:
 			state = states.SLAM
-			attack_ip = true
+			$Shield/shield_hitbox.disabled = false
+			$slam_timer.start()
+		
+		if state == states.BASH and $bash_timer.is_stopped():
+			$bash_timer.start()
 	
 		handle_states()
 
@@ -54,20 +61,25 @@ func handle_physics(delta):
 func handle_states():
 	match state:
 		states.IDLE:
+			speed = 0.0
 			anim.play("idle")
 		states.CHARGE:
-			speed = 30.0
+			speed = 80.0
 			anim.play("charge1")
-			$charge_timer.start()
-			if player_far == false or $charge_timer.is_stopped():
-				anim.play("bash1")
-				$bash_timer.start()
+			if player_far == false:
+				state = states.BASH
+		states.BASH:
+			speed = 25.0
+			anim.play("bash1")
 		states.SLAM:
+			speed = 0.0
 			if anim.flip_h == true:
-				$HitboxComponent2/shield_hitbox.rotation_degrees = 180
+				$Shield/shield_hitbox.rotation_degrees = 180
 			else:
-				$HitboxComponent2/shield_hitbox.rotation_degrees = 0
+				$Shield/shield_hitbox.rotation_degrees = 0
 			anim.play("slam1")
+			$shield_collision.set_collision_layer_value(1, false)
+			$shield_collision.set_collision_mask_value(1, false)
 
 func handle_flipping():
 	
@@ -82,11 +94,11 @@ func handle_flipping():
 	
 func _on_detection_area_body_entered(body):
 	if body.is_in_group("player"):
-		player_far = true
+		player_far = false
 
 func _on_detection_area_body_exited(body):
 	if body.is_in_group("player"):
-		player_far = false
+		player_far = true
 	
 	
 func _on_health_component_death():
@@ -101,7 +113,7 @@ func _on_death_timer_timeout():
 	new_soul.global_position = global_position
 	new_soul.experience = experience
 	new_soul.hp = soul_hp
-	loot_base.add_child(new_soul)
+	#loot_base.add_child(new_soul)
 	queue_free()
 
 func _on_hitbox_component_hit():
@@ -122,14 +134,25 @@ func _on_shield_area_entered(area):
 func _on_hurt_timer_timeout():
 	$AnimatedSprite2D/ColorRect.visible = false
 
-
 func _on_flip_timer_timeout():
 	if flip == false:
 		anim.flip_h = false
 	else:
 		anim.flip_h = true
 
-
 func _on_bash_timer_timeout():
-	attack_ip = false
+	state = states.IDLE
+	print("bash done")
+	$Shield/shield_hitbox.disabled = true
 	anim.play("idle")
+	$attack_cooldown.start()
+
+func _on_slam_timer_timeout():
+	state = states.IDLE
+	$shield_collision.set_collision_layer_value(1, true)
+	$shield_collision.set_collision_mask_value(1, true)
+	$Shield/shield_hitbox.disabled = true
+	$attack_cooldown.start()
+
+func _on_attack_cooldown_timeout():
+	attack_ip = false
